7b5c6fc637efc63b0c9931d49652ebb1e173f2e8
彬 <17661977890@163.com>
2019-07-19 13:40:08 +0800

    hbase-start

---
+++ src/main/java/com/example/hbasestart/HbaseStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.hbasestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbaseStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbaseStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hbasestart/HbaseStartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.hbasestart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HbaseStartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
